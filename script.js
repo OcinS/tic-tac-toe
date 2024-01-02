4ccd530242ebf330fbd6edfef700d789cad79fc9
OcinS <nicodsison@gmail.com>
2024-01-02 22:29:36 +0800

    Update HTML, CSS and JS files

--- script.js
+++ script.js
@@ -1,60 +1,114 @@
-let gameBoard = {
-    row1: [``,``,``],
-    row2: [``,``,``],
-    row3: [``,``,``]
-};
+gameModule = (function() {
 
-let currentPlayer = `X`;
+    const cells = document.querySelectorAll(`.cell`);
+    const statusText = document.querySelector(`#statusText`);
+    const restartButton = document.querySelector(`#restartButton`);
 
-const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+    let currentPlayer = `X`;
+    let running = false;
 
+    initializeGame();
 
-function round() {
+    function initializeGame() {
 
-        let rowNumber = Number(prompt(`What row do you choose? (Select 1 to 3 only)`));
-        let columnNumber = Number(prompt(`What column do you choose? (Select 1 to 3 only)`));
-    
-        console.log(`You choose row ${rowNumber} and column ${columnNumber}`);
-    
-        switch (columnNumber) {
-            case 1:
-                columnNumber = 0;
+        cells.forEach(cell => cell.addEventListener(`click`, function(e) {
+
+            const cellArray = e.target.parentNode.children;
+            const cellIndex = Array.from(cellArray).indexOf(cell);
+
+            if (gameBoard[cellIndex] !== "" || !running) {
+                return;
+            };
+
+            updateCell(this, cellIndex);
+            checkWinner();
+        }));
+
+        statusText.textContent = `${currentPlayer}'s Turn`;
+        statusText.style.color = "var(--blue-color)";
+
+        restartButton.addEventListener(`click`, restartGame);
+
+        running = true;
+
+    };
+
+    function updateCell(cell, index) {
+        gameBoard[index] = currentPlayer;
+        cell.textContent = currentPlayer;
+        
+        if (currentPlayer === "X") {
+            cell.style.color = "var(--blue-color)";
+        } else {
+            cell.style.color = "var(--purple-color)";
+        }
+    };
+
+    function changePlayer() {
+        currentPlayer = (currentPlayer === `X`) ? `O` : `X`;
+        statusText.textContent = `${currentPlayer}'s Turn`;
+        if (currentPlayer === "X") {
+            statusText.style.color = "var(--blue-color)";
+        } else {
+            statusText.style.color = "var(--purple-color)";
+        }
+    };
+
+    function checkWinner() {
+        let roundWon = false;
+
+        for (let i = 0; i < winningConditions.length; i++) {
+
+            const condition = winningConditions[i];
+
+            const cellA = gameBoard[condition[0]];
+
+            const cellB = gameBoard[condition[1]];
+
+            const cellC = gameBoard[condition[2]];
+
+            if (cellA === "" || cellB === "" || cellC === "") {
+                continue
+            };
+
+            if (cellA === cellB && cellB === cellC) {
+                roundWon = true;
                 break;
-            case 2:
-                columnNumber = 1;
-                break;
-            case 3:
-                columnNumber = 2;
-                break;
-        };
-    
-        switch (rowNumber) {
-            case 1:
-                gameBoard.row1[columnNumber] = `${currentPlayer}`;
-                break;
-            case 2:
-                gameBoard.row2[columnNumber] = `${currentPlayer}`;
-                break;
-            case 3:
-                gameBoard.row3[columnNumber] = `${currentPlayer}`;
-                break;
-        };
-    
-        console.table(gameBoard);
+            }
+        }
 
-        changePlayer()
-    
-};
+        if (roundWon) {
+            statusText.textContent = `${currentPlayer} Wins!`;
+            running = false;
+        } 
+        
+        else if (!gameBoard.includes("")) {
+            statusText.textContent = `It's a Tie!`;
+            running = false;
+        }
 
-function changePlayer() {
-    currentPlayer = currentPlayer === `X` ? `O` : `X`;
-};
+        else {
+            changePlayer();
+        }
+    };
+
+    function restartGame() {
+        gameBoard = ["", "", "", "", "", "", "", "", ""]
+        cells.forEach(cell => cell.textContent = "");
+        currentPlayer = `X`;
+        statusText.textContent = `${currentPlayer}'s Turn`;
+        running = true;
+    };
+
+})();
